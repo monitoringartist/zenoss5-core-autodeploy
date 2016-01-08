34ef2b9f5ed40cb29a8c8bd6e74fb44efd9672b5
Jan Garaj <jan.garaj@gmail.com>
2016-01-08 16:27:47 +0000

    Ubuntu disabled

--- core-autodeploy.sh
+++ core-autodeploy.sh
@@ -152,6 +152,11 @@ if [ -f /etc/redhat-release ]; then
     hostos="redhat"
 # Check for Ubuntu
 elif grep -q "Ubuntu" /etc/issue; then
+
+    # Sorry guys - Ubuntu is disabled atm
+    echo -e "${yellow}Combination Ubuntu and Zenoss/Control Center has a installation issue. Search ubuntu in https://jira.zenoss.com/ ${endColor}"
+    exit 1
+    
     if ! grep -q "14.04" /etc/issue; then
         echo -e $notsupported
         curl -ks -o /dev/null "http://www.google-analytics.com/r/collect?v=1&tid=UA-68890375-1&cid=${cid}&t=event&ec=Installation&ea=Error&el=Unsupported%20Ubuntu&ev=1&dp=%2F&dl=http%3A%2F%2Fgithub.com%2Fmonitoringartist%2Fzenoss5-core-autodeploy" &> /dev/null
